d652e88771e292326f2c022b6f3a3851c3c52913
mghmay <mghmay@gmail.com>
2022-04-12 17:46:21 +0100

    update

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,5 @@
 const clientID = '71f1f3daefe64e5fadfc2efb9e38536d';
-const redirectURI = 'insidious-force.surge.sh';
+const redirectURI = 'https://www.insidious-force.surge.sh';
 let accessToken;
 
 const Spotify = {
