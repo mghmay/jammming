b2e79498481d651718ee46546ae7c52363a19acd
mghmay <mghmay@gmail.com>
2022-04-13 14:28:48 +0100

    added default and clearname function to playlist component

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -9,7 +9,6 @@ export default class Playlist extends Component {
     // we need to bind methods in order to be able to call them in the rendering process  
     this.handleNameChange = this.handleNameChange.bind(this);
   }
-
   //we call the event listener because we are waiting for the user to update the name
   handleNameChange(event) {
     //.target.value refers to what is actually written in the field
@@ -17,12 +16,22 @@ export default class Playlist extends Component {
     //we can't actually use this prop directly in the render because it requires an argument
     this.props.onNameChange(name);
   }
+  setDefault(e) {
+    if (e.target.value === '') {
+      e.target.value = 'New playlist';
+    }
+  }
+  clearName(e) {
+    if (e.target.value === 'New playlist') {
+      e.target.value = ''
+    }
+  }
   // onremove and onsave don't need to be bound because they don't require arguments
   // this means they don't actually need to be called, their values are passed automatically
   render() {
     return (
       <div className="Playlist">
-        <input value = {this.props.playlistName || 'New playlist'} onChange={this.handleNameChange} />
+        <input value = {this.props.playlistName || 'New playlist'} onBlur={this.setDefault} onFocus={this.clearName} onChange={this.handleNameChange} />
         <TrackList 
           onRemove={this.props.onRemove}
           tracks={this.props.playlistTracks}
